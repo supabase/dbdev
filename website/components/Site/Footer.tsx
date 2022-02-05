45225a53bc7abad5f070a43b1eaa4a8fa21ba8da
Copple <10214025+kiwicopple@users.noreply.github.com>
2022-02-05 19:10:35 +0100

    cleans up footer links

--- website/components/Site/Footer.tsx
+++ website/components/Site/Footer.tsx
@@ -1,10 +1,10 @@
+import { SITE_INFO } from "../../lib/constants";
 
-/* This example requires Tailwind CSS v2.0+ */
 const navigation = {
   main: [
-    { name: 'Packages', href: '#' },
-    { name: 'Docs', href: '#' },
-    { name: 'Partners', href: '#' },
+    { name: 'Packages', href: '/' },
+    { name: 'Docs', href: '/' },
+    { name: 'Pricing', href: '/' },
   ],
   social: [
     {
@@ -75,16 +75,10 @@ export default function Footer() {
   return (
     <footer className="border-t">
       <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
-        <nav
-          className="-mx-5 -my-2 flex flex-wrap justify-center"
-          aria-label="Footer"
-        >
+        <nav className="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
           {navigation.main.map((item) => (
             <div key={item.name} className="px-5 py-2">
-              <a
-                href={item.href}
-                className="text-base text-gray-500 hover:text-gray-900"
-              >
+              <a href={item.href} className="text-base text-gray-500 hover:text-gray-900">
                 {item.name}
               </a>
             </div>
@@ -92,20 +86,14 @@ export default function Footer() {
         </nav>
         <div className="mt-8 flex justify-center space-x-6">
           {navigation.social.map((item) => (
-            <a
-              key={item.name}
-              href={item.href}
-              className="text-gray-400 hover:text-gray-500"
-            >
+            <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
               <span className="sr-only">{item.name}</span>
               <item.icon className="h-6 w-6" aria-hidden="true" />
             </a>
           ))}
         </div>
-        <p className="mt-8 text-center text-base text-gray-400">
-          &copy; 2020 Workflow, Inc. All rights reserved.
-        </p>
+        <p className="mt-8 text-center text-base text-gray-400">{SITE_INFO.copyright}</p>
       </div>
     </footer>
-  );
+  )
 }
--- website/lib/constants.ts
+++ website/lib/constants.ts
@@ -2,4 +2,5 @@ export const SITE_INFO = {
   name: 'database.dev',
   description:
     'database.dev is a database package manager, like NPM for NodeJS, or Cargo for Rust.',
+  copyright: '© 2020 Supabase, Inc. All rights reserved.',
 }
