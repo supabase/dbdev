94c7316370c61a151faa664864f3f3b0f11a4143
Alaister Young <a@alaisteryoung.com>
2023-02-23 00:39:55 +1100

    adds example org name input

--- website/pages/organizations/new.tsx
+++ website/pages/organizations/new.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router'
+import { useState } from 'react'
 import AuthenticatedLayout from '~/components/layouts/AuthenticatedLayout'
 import { useOrganizationCreateMutation } from '~/data/organizations/organization-create-mutation'
 import { useProfilePrefetch } from '~/data/profiles/profile-query'
@@ -25,13 +26,15 @@ const NewOrganizationPage: NextPageWithLayout = () => {
     },
   })
 
+  const [handle, setHandle] = useState('')
+
   const handleCreate = () => {
     if (!user) return
 
     mutate({
-      handle: 'myorg3',
-      displayName: 'My Org',
-      contactEmail: 'alaisterody@gmail.com',
+      handle: handle,
+      displayName: handle,
+      contactEmail: 'alaister@supabase.io',
       userId: user.id,
     })
   }
@@ -39,6 +42,14 @@ const NewOrganizationPage: NextPageWithLayout = () => {
   return (
     <div>
       <h1>New Organization</h1>
+
+      <label htmlFor="handle">Handle</label>
+      <input
+        id="handle"
+        type="text"
+        value={handle}
+        onChange={(e) => setHandle(e.target.value)}
+      />
 
       <button onClick={handleCreate}>Save</button>
     </div>
