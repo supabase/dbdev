8e161e2145af11b2c361f8db607f3f4cb1ac5e54
Joshen Lim <joshenlimek@gmail.com>
2023-04-14 22:55:33 +0700

    Fix blog link

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -55,7 +55,7 @@ const IndexPage: NextPageWithLayout = () => {
               Getting started
             </Link>
             <Link
-              href="https://supabase.com/blog/dbdev-package-manager"
+              href="https://supabase.com/blog/dbdev"
               className="flex items-center px-4 py-2 space-x-2 text-gray-500 transition bg-white border rounded-md group dark:bg-transparent dark:border-slate-500 dark:text-gray-300 dark:hover:text-gray-100 dark:hover:border-slate-400 hover:text-gray-700 hover:border-gray-400"
             >
               <span>Read the blog post</span>
