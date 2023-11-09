c7a81982e887d5deb0e07dc93e3789bcbe730a3b
Raminder Singh <romi_ssk@yahoo.co.in>
2023-11-09 15:35:59 +0530

    show default version

--- cli/src/commands/list.rs
+++ cli/src/commands/list.rs
@@ -6,14 +6,22 @@ use sqlx::PgConnection;
 use crate::commands::install::update_paths;
 
 pub(crate) async fn list(conn: &mut PgConnection) -> anyhow::Result<()> {
+    let available_extension_versions = available_extensions_versions(conn).await?;
     let available_extensions = available_extensions(conn).await?;
 
-    for (extension_name, versions) in available_extensions {
+    for (extension_name, versions) in available_extension_versions {
+        let default_version = available_extensions.get(&extension_name);
+
         println!("{extension_name}");
         println!("  available versions:");
-        for version in versions {
-            println!("    {version}");
+        for version in &versions {
+            print!("    {version}");
+            if Some(version) == default_version {
+                print!(" (default)");
+            }
+            println!();
         }
+
         println!("  available update paths:");
         let update_paths = update_paths(conn, &extension_name).await?;
         if update_paths.is_empty() {
@@ -30,15 +38,15 @@ pub(crate) async fn list(conn: &mut PgConnection) -> anyhow::Result<()> {
 }
 
 #[derive(sqlx::FromRow)]
-struct ExtensionRow {
+struct AvailableExtensionVersion {
     name: String,
     version: String,
 }
 
-async fn available_extensions(
+async fn available_extensions_versions(
     conn: &mut PgConnection,
 ) -> anyhow::Result<HashMap<String, Vec<String>>> {
-    let mut rows = sqlx::query_as::<_, ExtensionRow>(
+    let mut rows = sqlx::query_as::<_, AvailableExtensionVersion>(
         "select name, version from pgtle.available_extension_versions()",
     )
     .fetch(conn);
@@ -50,4 +58,24 @@ async fn available_extensions(
     }
 
     Ok(available_extensions)
+}
+
+#[derive(sqlx::FromRow)]
+struct AvailableExtension {
+    name: String,
+    default_version: String,
+}
+
+async fn available_extensions(conn: &mut PgConnection) -> anyhow::Result<HashMap<String, String>> {
+    let mut rows = sqlx::query_as::<_, AvailableExtension>(
+        "select name, default_version from pgtle.available_extensions()",
+    )
+    .fetch(conn);
+
+    let mut extensions = HashMap::new();
+    while let Some(row) = rows.try_next().await? {
+        extensions.insert(row.name, row.default_version);
+    }
+
+    Ok(extensions)
 }
