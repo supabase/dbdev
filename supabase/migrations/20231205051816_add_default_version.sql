61130e8697051586b9b537a5071a1d2ae6882ba0
Raminder Singh <romi_ssk@yahoo.co.in>
2023-12-05 13:38:00 +0530

    add default version to the app.packages table

---
+++ supabase/migrations/20231205051816_add_default_version.sql
@@ -0,0 +1,44 @@
+-- The original semver domain defined in 20220117141507_semver.sql doesn't allow null
+-- app.semver_struct values, but we need a nullable app.semver column for the new
+-- default_version_struct column in the app.packages table (see alter table app.package below).
+-- So we modify the `is_valid` function such that it returns true if the input version itself is
+-- null. All the existing tables where app.semver domain is used already have an additional
+-- non null constraint, so their behaviour doesn't change.
+create or replace function app.is_valid(version app.semver_struct)
+    returns boolean
+    immutable
+    language sql
+as $$
+    select (
+        version is null or (
+            version.major is not null
+            and version.minor is not null
+            and version.patch is not null
+        )
+    )
+$$;
+
+-- same definition as the original function defined in 20220117141507_semver.sql with the only
+-- difference being that this is marked strict. This is done so that the function returns null
+-- on null input instead of `..`
+create or replace function app.semver_to_text(version app.semver)
+    returns text
+    immutable
+    strict
+    language sql
+as $$
+    select
+        format('%s.%s.%s', version.major, version.minor, version.patch)
+$$;
+
+-- default version columns are nullable for backward compatibility with older clients
+alter table app.packages
+add column default_version_struct app.semver,
+add column default_version text generated always as (app.semver_to_text(default_version_struct)) stored;
+
+-- for now we set the default version to current latest version
+-- new client will allow users to set a specific default version in the control file
+update app.packages
+set default_version_struct = app.text_to_semver(pp.latest_version)
+from public.packages pp
+where packages.id = pp.id;
