7398c4a74629547f2dcb511e37f32057c4834d49
Raminder Singh <romi_ssk@yahoo.co.in>
2023-12-05 14:32:30 +0530

    update public.packages view to include default_version column

--- supabase/migrations/20231205051816_add_default_version.sql
+++ supabase/migrations/20231205051816_add_default_version.sql
@@ -42,3 +42,25 @@ update app.packages
 set default_version_struct = app.text_to_semver(pp.latest_version)
 from public.packages pp
 where packages.id = pp.id;
+
+create or replace view public.packages as
+    select
+        pa.id,
+        pa.package_name,
+        pa.handle,
+        pa.partial_name,
+        newest_ver.version as latest_version,
+        newest_ver.description_md,
+        pa.control_description,
+        pa.control_requires,
+        pa.created_at,
+        pa.default_version
+    from
+        app.packages pa,
+        lateral (
+            select *
+            from app.package_versions pv
+            where pv.package_id = pa.id
+            order by pv.version_struct
+            limit 1
+        ) newest_ver;
