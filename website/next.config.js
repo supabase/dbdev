1cdd0d6a5f7032b474282b53172ec0bd332fd878
Etienne Stalmans <etienne@supabase.io>
2024-12-30 15:08:32 +0100

    chore: add csp header
    Adds a CSP header to database.dev

--- website/next.config.js
+++ website/next.config.js
@@ -1,6 +1,33 @@
 /** @type {import('next').NextConfig} */
+
+const cspHeader = `
+  default-src 'self' https://api.database.dev/;
+  style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com/ https://fonts.google.com/;
+  img-src 'self' data: https://api.database.dev/storage/;
+  object-src 'none';
+  base-uri 'none';
+  frame-ancestors 'none';
+`
+
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          }
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
